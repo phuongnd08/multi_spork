fb92e5a620ba19aa6787dd68dce43b5235f46262
Phuong Nguyen <phuongnd08@gmail.com>
2012-02-19 11:06:52 +0700

    Change the way MultiSpork prefork and each_run work, remove debugging instruction

--- lib/multi_spork.rb
+++ lib/multi_spork.rb
@@ -8,20 +8,26 @@ module MultiSpork
   autoload :Configuration, "multi_spork/configuration"
 
   class << self
-    attr_accessor :each_run_block
-
     def prefork
       Spork.prefork do
         yield
-      end
-
-      if defined? ActiveRecord::Base
-        ActiveRecord::Base.connection.disconnect!
+        if defined? ActiveRecord::Base
+          ActiveRecord::Base.connection.disconnect!
+        end
       end
     end
 
-    def each_run &block
-      self.each_run_block = block
+    def each_run
+      Spork.each_run do
+        if defined?(ActiveRecord::Base) && defined?(Rails)
+          db_count = MultiSpork.config.runner_count
+          db_index = (Spork.run_count % db_count) + 1
+          config = YAML.load(ERB.new(Rails.root.join('config/database.yml').read).result)['test']
+          config["database"] += db_index.to_s
+          ActiveRecord::Base.establish_connection(config)
+        end
+        yield
+      end
     end
 
     def config
@@ -32,17 +38,4 @@ module MultiSpork
       yield config
     end
   end
-end
-
-Spork.each_run do
-  if defined?(ActiveRecord::Base) && defined?(Rails)
-    db_count = MultiSpork.config.runner_count
-    db_index = (Spork.run_count % db_count) + 1
-    config = YAML.load(ERB.new(Rails.root.join('config/database.yml').read).result)['test']
-    config["database"] += db_index.to_s
-    puts "Connecting to database with this config"
-    pp config
-    ActiveRecord::Base.establish_connection(config)
-  end
-  MultiSpork.each_run_block.call if MultiSpork.each_run_block
 end
